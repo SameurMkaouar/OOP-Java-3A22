6d2fbbd2dd683161c86581d5522762c85e31c269
SameurMkaouar <sameur.mkaouar@esprit.tn>
2024-10-01 12:49:38 +0100

    seance 4

---
+++ seance4/src/tn/esprit/gestionzoo/main/Main.java
@@ -0,0 +1,28 @@
+package tn.esprit.gestionzoo.main;
+import tn.esprit.gestionzoo.entities.*;
+public class Main {
+    public static void main(String[] args) {
+        Animal asad = new Animal("Carnivore", "Lion", 25, true);
+        Animal zarafa = new Animal("Herbivore","Giraffe", 15 , true);
+        Animal djeja = new Animal("Omnivore", "Chicken", 3,false);
+        Animal[] animals = {asad, zarafa,djeja};
+        Zoo herZoo = new Zoo("zoo de Marwa", "Kram");
+        Zoo myZoo = new Zoo("zoo de Sameur", "Aouina");
+        herZoo.addAnimal(asad);
+        herZoo.addAnimal(zarafa);
+        myZoo.addAnimal(djeja);
+        System.out.println(herZoo.toString());
+        System.out.println(asad.toString());
+        Zoo BESTZOO = herZoo.comparerZoo(myZoo,herZoo);
+        System.out.println("BEST ZOO: "+BESTZOO);
+        herZoo.supprimerAnimal(asad);
+        System.out.println(herZoo.toString());
+
+        /*
+        asad.afficher();
+        zarafa.afficher();
+        djeja.afficher();
+        */
+
+    }
+}
